853a60e92e525870c6951db16f0da8b4fcb124ce
Sahar Javadi <sahar.javadi.h@gmail.com>
2025-05-05 17:25:55 +0330

    edit commentRow

--- app/_components/commentsComponents/CommentRow.jsx
+++ app/_components/commentsComponents/CommentRow.jsx
@@ -27,7 +27,7 @@ function CommentRow({ comment, index }) {
     <Table.Row>
       <td>{toPersianDigits(index + 1)}</td>
       <td>{truncateText(content.text, 30)}</td>
-      <td>{user.name}</td>
+      <td>{user?.name ?? "سحر"}</td>
       <td>{toLocalDateShort(createdAt)}</td>
       <td>
         <span className={`badge ${commentStatus[status].className}`}>
