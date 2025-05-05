f8ae5310cb55f66886048255d6fa614cb92106e5
Sahar Javadi <sahar.javadi.h@gmail.com>
2025-05-05 17:45:39 +0330

    fix bugs

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -8,7 +8,6 @@ import {
 } from "./commentService";
 import { setCookiesOnReq } from "../_utils/setCookiesOnReq";
 import { revalidatePath } from "next/cache";
-import { getUserApi } from "./authService";
 
 export async function createComment(prevState, { formData, postId, parentId }) {
   const cookiesStore = cookies();
